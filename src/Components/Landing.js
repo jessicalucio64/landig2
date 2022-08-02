24b9181ba249a75d535bd02b85bda819f5d39ac2
jessicalucio64 <jessicafernandalucio4@gmail.com>
2022-08-02 00:08:20 -0500

    se empezo

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./Landing.css";
+import fondo from '../media/iconografia-hacienda-valbuena-escritorio.webp';
 
 const Landing = () => {
   return (
-    <div className='container__landing'>Landing</div>
+    <div className='container__landing'>
+        <img src={fondo} className='fondo__img' alt=""/>
+        <div className='titulo__container'>
+          <h1><span className='titulo'>HV</span><span className='titulo1'>HACIENDA VALBUENA</span></h1>
+        </div>
+        <div className='subtitulo__container'>
+          <h2>$i SE PUEDE VIVIR CON LUJO</h2>
+        </div>
+    </div>
   )
 }
 
